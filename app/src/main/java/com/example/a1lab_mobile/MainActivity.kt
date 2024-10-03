33439010c29d8bca07a8ade92bf91aed7b3e0dce
SomiLast <Lyutinkhay.bk.ru@gmail.com>
2024-10-04 01:01:23 +0700

    Проверка возраста с помощью if else и обработчик нажатия на кнопку

--- app/src/main/java/com/example/a1lab_mobile/MainActivity.kt
+++ app/src/main/java/com/example/a1lab_mobile/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.a1lab_mobile
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +19,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // Находим элементы интерфейса
+        val ageInput = findViewById<EditText>(R.id.editTextText)
+        val checkAgeButton = findViewById<Button>(R.id.button)
+        val messageTextView = findViewById<TextView>(R.id.textViewMessage)
 
+        // Устанавливаем обработчик нажатия на кнопку
+        checkAgeButton.setOnClickListener {
+            // Получаем введенное значение и проверяем его
+            val ageText = ageInput.text.toString()
+            val age = ageText.toIntOrNull()
+
+            if (age != null) {
+                // Проверяем возраст с помощью if-else
+                val message = when {
+                    age <= 20 -> "Вы слишком молоды!"
+                    age == 30 || age == 40 || age == 50 || age == 60 -> "Поздравляем с повышением!"
+                    age == 65 -> "Преподносим вам золотые часы!"
+                    age > 65 -> "Вы слишком стары!"
+                    else -> "Продолжайте накапливать опыт!"
+                }
+
+                // Выводим сообщение в TextView
+                messageTextView.text = message
+            }
+        }
     }
 }
